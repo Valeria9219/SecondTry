c858e8612f9e96dde231e1234caf1e60477537ab
“Valeria” <kaleriaash@gmail.com>
2022-11-04 16:26:02 +0300

    v 4.2

---
+++ js/einstein.js
@@ -0,0 +1,5 @@
+'use strict'
+const myWeight = 5;
+const speedLight = 3e8;
+const E = myWeight * speedLight ** 2
+console.log(E)
--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert('hi')
+'use strict'
+const productName = prompt('Введите название товара', 'Apple');
+const productCount = +prompt('Введите количество товара', 0);
+const productCategory = prompt('Введите название товара', 'Fruct');
+const productPrice = +prompt('Введите количество товара', 10);
+
+console.log(Number.isFinite(productCount), Number.isFinite(productPrice))
+console.log(`На складе ${productCount} единицы товара ${productName} на сумму ${productCount * productPrice} рублей`);
